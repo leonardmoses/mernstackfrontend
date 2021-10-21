ea0e53b1a45bde906617cbf232551f6e3f792592
leonardmoses <leonard.moses@anemote.com>
2021-10-20 20:58:11 -0400

    Added Delete button and functionality. Added netlify.toml in preparation for frontend deployment.

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -2,9 +2,9 @@ import { useState } from "react"
 
 function Show(props) {
 
-    const id = props.match.params.id
-    const people = props.people
-    const person = people.find(p => p._id === id)
+    const id = props.match.params.id;
+    const people = props.people;
+    const person = people.find(p => p._id === id);
 
     // state for form
     const [editForm, setEditForm] = useState(person);
@@ -17,42 +17,52 @@ function Show(props) {
     // handlesubmit for form
     const handleSubmit = event => {
         event.preventDefault();
-        props.updatePeople(editForm, person._id);
+        props.updatePeople(editForm);
+        // redirect people back to index
+        props.history.push("/");
+    }
+
+    //delete function for person
+    const removePerson = () => {
+        props.deletePeople(person._id);
         // redirect people back to index
         props.history.push("/");
     }
 
     return (
         <div className="person">
-          <h1>{person.name}</h1>
-          <h2>{person.title}</h2>
-          <img src={person.image} alt={person.name} />
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              value={editForm.name}
-              name="name"
-              placeholder="name"
-              onChange={handleChange}
-            />
-            <input
-              type="text"
-              value={editForm.image}
-              name="image"
-              placeholder="image URL"
-              onChange={handleChange}
-            />
-            <input
-              type="text"
-              value={editForm.title}
-              name="title"
-              placeholder="title"
-              onChange={handleChange}
-            />
-            <input type="submit" value="Update Person" />
-          </form>
+            <h1>{person.name}</h1>
+            <h2>{person.title}</h2>
+            <img src={person.image} alt={person.name} />
+            <button id="delete" onClick={removePerson}>
+                DELETE
+            </button>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    value={editForm.name}
+                    name="name"
+                    placeholder="name"
+                    onChange={handleChange}
+                />
+                <input
+                    type="text"
+                    value={editForm.image}
+                    name="image"
+                    placeholder="image URL"
+                    onChange={handleChange}
+                />
+                <input
+                    type="text"
+                    value={editForm.title}
+                    name="title"
+                    placeholder="title"
+                    onChange={handleChange}
+                />
+                <input type="submit" value="Update Person" />
+            </form>
         </div>
-      );
-    }
-    
-    export default Show;
+    )
+}
+
+export default Show;
